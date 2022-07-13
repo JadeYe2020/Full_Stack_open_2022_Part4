4c21f81b5818a6d3f7d9de5d4eef5fe7d0f10352
Jade Ye <w0430394@nscc.ca>
2022-07-13 17:00:07 -0300

    Finish 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,29 @@ const totalLikes = (blogs) => {
   } , 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  
+  let maxLikes = 0
+  blogs.forEach(blog => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes
+    }
+  })
+
+  const favBlog = blogs.find(blog => blog.likes === maxLikes)
+
+  return {
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
